492e39e0f6dea98c6bc26b2fab0109c4995106dc
gaozhangmin <gaozhangmin@gmail.com>
2026-05-23 10:35:53 +0800

    fix

--- version.mjs
+++ version.mjs
@@ -1,18 +1,21 @@
+import dayjs from 'dayjs'
+import { execSync } from 'node:child_process'
 import fs from 'node:fs'
 import semver from 'semver'
 
 process.env.TZ = 'Asia/Shanghai'
 const packageJsonStr = fs.readFileSync('./package.json').toString()
 try {
+  const tmp = `3.${dayjs().format('YY.MDDHH')}`
   const packageJson = JSON.parse(packageJsonStr)
-  const parsed = semver.parse(packageJson.version)
-  if (!parsed) {
-    throw new Error(`Invalid package version: ${packageJson.version}`)
+  if (semver.gt(tmp, packageJson.version)) {
+    packageJson.version = tmp
+    console.info('版本升级为' + packageJson.version)
+    fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2))
+    execSync(`git add package.json`)
   }
-  packageJson.version = `${parsed.major}.${parsed.minor}.${parsed.patch + 1}`
-  console.info('版本升级为' + packageJson.version)
-  fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2) + '\n')
 } catch (e) {
   console.error('处理package.json失败，请重试', e.message)
   process.exit(1)
 }
+
